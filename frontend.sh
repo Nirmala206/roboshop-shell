94537098260e0b33ecb690c42e157fbb6391be01
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-02 02:07:16 +0530

    Added simple variable program

--- frontend.sh
+++ frontend.sh
@@ -10,7 +10,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying the Nginx config for Roboshop\e[0m"
-cp ${code_dir}configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx

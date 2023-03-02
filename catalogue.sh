47b891adb90b0ee3a4172dbdb51eb52155c3f104
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-03 02:37:45 +0530

    Added common.sh to catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,25 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
+print_head "Configure NodeJs Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+print_head "Install NodeJs"
+yum install nodejs -y &>>${log_file}
+useradd roboshop &>>${log_file}
+mkdir /app &>>${log_file}
+
+rm -rf /app/* &>>${log_file}
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
+unzip /tmp/catalogue.zip &>>${log_file}
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+npm install &>>${log_file}
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.devops2023.tech </app/schema/catalogue.js
+systemctl daemon-reload &>>${log_file}
+systemctl enable catalogue &>>${log_file}
+systemctl start catalogue &>>${log_file}
+
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+yum install mongodb-org-shell -y &>>${log_file}
+mongo --host mongodb.devops2023.tech </app/schema/catalogue.js &>>${log_file}

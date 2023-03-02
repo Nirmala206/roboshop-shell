3a4b475e204169c0271f92d47321e952398bcb9f
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-03 02:20:32 +0530

    Added common.sh to mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,16 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
+source common.sh
 
-print_head(){
-  echo -e "\e[36m$1\e[0m"
-}
 
+print_head "Setup MongoDB repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install MongoDB"
 yum install mongodb-org -y
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+
+print_head "Start MongoDB service"
 systemctl start mongod
 
 #udate /etc/mongod.conf file from 127.0.0.1 to 0.0.0.0

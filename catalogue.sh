583c281ee639434ecac953f88503e6c88e487fa3
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-03 13:28:47 +0530

    Added logfile and print_head to catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -5,21 +5,43 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head "Install NodeJs"
 yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop User"
 useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
 mkdir /app &>>${log_file}
 
+print_head "Delete Old Content"
 rm -rf /app/* &>>${log_file}
+
+print_head "Download App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
+
+print_head "Extracting App Content"
 unzip /tmp/catalogue.zip &>>${log_file}
 
+print_head "Installing NodeJS Dependencies"
 npm install &>>${log_file}
+
+print_head "Copy SystemD Service File"
 cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${log_file}
+
+print_head "Start  Catalogue Service"
 systemctl start catalogue &>>${log_file}
 
+print_head "Copy MongoDB Repo File"
 cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install MongoDB Client"
 yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
 mongo --host mongodb.devops2023.tech </app/schema/catalogue.js &>>${log_file}

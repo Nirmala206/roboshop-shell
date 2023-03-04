f6d3a1a2163bafdaff41c5ec71d3fbc89101942a
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-04 17:33:17 +0530

    Added echo command for exit status

--- frontend.sh
+++ frontend.sh
@@ -2,24 +2,30 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+echo $?
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
 print_head "Dowloading the frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Extracting the downloaded frontend content"
 cd /usr/share/nginx/html &>>${log_file}
 unzip /tmp/frontend.zip &>>${log_file}
+echo $?
 
 print_head "Copying the Nginx config for Roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+echo $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+echo $?
 
 print_head "Starting nginx"
 systemctl start nginx &>>${log_file}
-
+echo $?
 

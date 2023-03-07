46373230acf5eb597bb0acc9af3ab0500629b023
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-07 20:22:21 +0530

    Added sgipping sevice and .sh files

--- common.sh
+++ common.sh
@@ -16,62 +16,7 @@ status_check() {
   fi
 }
 
-schema_setup() {
-
-  if [ "${schema_type}" == "mongo" ]; then
-    print_head "Copy MongoDB Repo File"
-    cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-    status_check $?
-
-    print_head "Install MongoDB Client"
-    yum install mongodb-org-shell -y &>>${log_file}
-    status_check $?
-
-    print_head "Load Schema"
-    mongo --host mongodb.devops2023.tech </app/schema/${component}.js &>>${log_file}
-    status_check $?
-  fi
-}
-
-nodejs() {
-  print_head "Configure NodeJs Repo"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-  status_check $?
-
-  print_head "Install NodeJs"
-  yum install nodejs -y &>>${log_file}
-  status_check $?
-
-  print_head "Create Roboshop User"
-  id roboshop &>>${log_file}
-  if [ $? -ne 0 ]; then
-   useradd roboshop &>>${log_file}
-  fi
-  status_check $?
-
-  print_head "Create Application Directory"
-  if [ ! -d /app  ]; then
-   mkdir /app &>>${log_file}
-  fi
-  status_check $?
-
-  print_head "Delete Old Content"
-  rm -rf /app/* &>>${log_file}
-  status_check $?
-
-  print_head "Download App Content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  status_check $?
-  cd /app
-
-  print_head "Extracting App Content"
-  unzip /tmp/${component}.zip &>>${log_file}
-  status_check $?
-
-  print_head "Installing NodeJS Dependencies"
-  npm install &>>${log_file}
-  status_check $?
-
+systemd_setup() {
   print_head "Copy SystemD Service File"
   cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
   status_check $?
@@ -87,7 +32,97 @@ nodejs() {
   print_head "Start  Catalogue Service"
   systemctl start ${component} &>>${log_file}
   status_check $?
+}
+
+schema_setup() {
+
+  if [ "${schema_type}" == "mongo" ]; then
+    print_head "Copy MongoDB Repo File"
+    cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+    status_check $?
+
+    print_head "Install MongoDB Client"
+    yum install mongodb-org-shell -y &>>${log_file}
+    status_check $?
+
+    print_head "Load Schema"
+    mongo --host mongodb.devops2023.tech </app/schema/${component}.js &>>${log_file}
+    status_check $?
+  elif [ "${schema_type}" == "mysql" ]; then
+    print_head "Install MySQL Client"
+    yum install mysql -y &>>${log_file
+    status_check $?
+
+    print_head "Load Schema"
+    mysql -h mysql.devops2023.tech -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+
+    fi
+}
+
+app_prereq_setup() {
+  print_head "Create Roboshop User"
+    id roboshop &>>${log_file}
+    if [ $? -ne 0 ]; then
+     useradd roboshop &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Create Application Directory"
+    if [ ! -d /app  ]; then
+     mkdir /app &>>${log_file}
+    fi
+    status_check $?
+
+    print_head "Delete Old Content"
+    rm -rf /app/* &>>${log_file}
+    status_check $?
+
+    print_head "Download App Content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    status_check $?
+    cd /app
+
+    print_head "Extracting App Content"
+    unzip /tmp/${component}.zip &>>${log_file}
+    status_check $?
+}
+
+nodejs() {
+  print_head "Configure NodeJs Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install NodeJs"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Installing NodeJS Dependencies"
+  npm install &>>${log_file}
+  status_check $?
+
 
   schema_setup
+  systemd_setup
 
+}
+
+java() {
+
+  print_head "Delete Old Content"
+  yum install maven -y &>>${log_file}
+  status_check $?
+
+  app_prereq_setup
+
+  print_head "Download Dependencies & Packages"
+  mvn clean package &>>${log_file} &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+  #Schema Setup Function
+  schema_setup
+
+  #SystemD Function
+  systemd_setup
 }

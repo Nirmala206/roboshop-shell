7cbc6a77aaebd451c7413483fe1ee056fc2e94bc
Nirmala <nirmalapolisetty206@gmail.com>
2023-03-08 00:36:16 +0530

    Added RabbitMq.sh file

---
+++ rabbitmq.sh
@@ -0,0 +1,44 @@
+source common.sh
+
+mysql_root_password=$1
+
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+  exit 1
+fi
+
+print_head "Setup Erlang repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
+status_check $?
+
+#print_head "Copy SystemD Service File"
+#yum install erlang -y &>>${log_file}
+#status_check $?
+
+print_head "Setup RabbitMQ Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
+status_check $?
+
+print_head "Install Erlang & RabbitMQ"
+yum install rabbitmq-server erlang -y &>>${log_file}
+status_check $?
+
+print_head "Enable RabbitMQ Service"
+systemctl enable rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Start RabbitMQ Service"
+systemctl start rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Copy SystemD Service File"
+rabbitmqctl add_user roboshop ${mysql_root_password} &>>${log_file}
+status_check $?
+
+#print_head "Copy SystemD Service File"
+#rabbitmqctl set_user_tags roboshop administrator &>>${log_file}
+#status_check $?
+
+print_head "Configure Permissions for App User"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
+status_check $?
